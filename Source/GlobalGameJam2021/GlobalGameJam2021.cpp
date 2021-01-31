095c8677f1974c7f49c1a1ea3ad01ee821deba18
hassanhalabi1 <50659517+hassanhalabi1@users.noreply.github.com>
2021-01-31 18:57:39 +0100

    deniere manip

---
+++ Source/GlobalGameJam2021/GlobalGameJam2021.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GlobalGameJam2021.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GlobalGameJam2021, "GlobalGameJam2021" );
